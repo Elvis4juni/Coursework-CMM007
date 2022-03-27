c2ba99b1f84314116526fed3de0d0169c92fa264
Elvis4juni <74566018+Elvis4juni@users.noreply.github.com>
2022-03-28 00:30:19 +0100

    ok

--- indexa.php
+++ indexa.php
@@ -2,21 +2,91 @@
 
 <!DOCTYPE html>
 <html>
+	
 <head>
   <title>Registration system PHP and MySQL</title>
   <link rel="stylesheet" type="text/css" href="style.css">
 </head>
+<style>
+	/* Bordered form */
+form {
+  border: 3px solid #f1f1f1;
+  background-color: darkolivegreen;
+}
+
+/* Full-width inputs */
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+  background-color: plum;
+}
+
+/* Set a style for all buttons */
+button {
+  background-color: #04AA6D;
+  color: pink;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+
+/* Add a hover effect for buttons */
+button:hover {
+  opacity: 0.8;
+}
+
+/* Extra style for the cancel button (red) */
+.cancelbtn {
+  width: auto;
+  padding: 10px 18px;
+  background-color: #f44336;
+}
+
+/* Center the avatar image inside this container */
+.imgcontainer {
+  text-align: center;
+  margin: 24px 0 12px 0;
+}
+
+/* Avatar image */
+img.avatar {
+  width: 40%;
+  border-radius: 50%;
+}
+
+/* Add padding to containers */
+.container {
+  padding: 16px;
+}
+
+
+
+
+.body{
+	background-color: #f44336;
+	background-image: url();
+}
+</style>
 <body>
-  <div class="header">
+  <div class="container">
   	<h2>Login</h2>
   </div>
 	 
   <form method="post" action="login.php">
+  <div class="imgcontainer">
+    <img src="images/art sketch 2.jpeg" style="width:500px;height:200px">
+  </div>
   	<div class="input-group">
   		<label>Username</label>
   		<input type="text" name="username">
   	</div>
-  	<div class="input-group">
+  	<div class="container">
   		<label>Password</label>
   		<input type="password" name="password">
   	</div>
